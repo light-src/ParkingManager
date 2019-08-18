855d09e6b2243e9f500f35319089c23342ffdd68
Kwangwon Choi <rhkddnjs99@hotmail.com>
2019-08-19 00:49:31 +0900

    Add ChildEventListener to get List from firebase

--- app/src/main/java/com/cgwprj/parkingmanager/Views/Fragments/MainFragment.java
+++ app/src/main/java/com/cgwprj/parkingmanager/Views/Fragments/MainFragment.java
@@ -1,28 +1,36 @@
 package com.cgwprj.parkingmanager.Views.Fragments;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.cgwprj.parkingmanager.Data.UserData;
+import com.cgwprj.parkingmanager.Models.CarInfo;
 import com.cgwprj.parkingmanager.R;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.List;
 
 public class MainFragment extends android.support.v4.app.Fragment{
 
+    FirebaseUser firebaseUser;
+    private DatabaseReference mDataBase;
+    private List<CarInfo> carInfoList;
+
     public MainFragment() {
-        // Required empty public constructor
+        firebaseUser = UserData.getInstance().getUser();
+        mDataBase = FirebaseDatabase.getInstance().getReference();
+        mDataBase.addChildEventListener(childEventListener);
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @return A new instance of fragment MainFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static MainFragment newInstance() {
         MainFragment fragment = new MainFragment();
         return fragment;
@@ -40,13 +48,31 @@ public class MainFragment extends android.support.v4.app.Fragment{
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-    }
+    ChildEventListener childEventListener = new ChildEventListener() {
+        @Override
+        public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-    }
+        }
+
+        @Override
+        public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+        }
+
+        @Override
+        public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+        }
+
+        @Override
+        public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+        }
+    };
+
 }
