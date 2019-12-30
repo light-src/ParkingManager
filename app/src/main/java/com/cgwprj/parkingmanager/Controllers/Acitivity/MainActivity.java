a51bdb0e10e952f6174e83e10ce2e85298dc6160
Kwangwon Choi <rhkddnjs99@hotmail.com>
2019-12-31 01:04:44 +0900

    Bug fix * change the way to check selected car because of listview in android has mulfunction.

--- app/src/main/java/com/cgwprj/parkingmanager/Controllers/Acitivity/MainActivity.java
+++ app/src/main/java/com/cgwprj/parkingmanager/Controllers/Acitivity/MainActivity.java
@@ -191,7 +191,7 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            replaceFragment(CleanFragment.newInstance());
+            replaceFragment(CleanFragment.newInstance(this));
             return true;
         }
 
