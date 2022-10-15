6e5036bfbd894da8e223a152d45d40948d1def09
최광원 <choegwang-won@choegwang-won-ui-MacBookAir.local>
2022-10-15 11:51:16 +0900

    fix FEE 400 -> 500

--- app/src/main/java/com/cgwprj/parkingmanager/Utils/FEE.java
+++ app/src/main/java/com/cgwprj/parkingmanager/Utils/FEE.java
@@ -4,7 +4,7 @@ public enum FEE {
     FEE_FREE_TIME(10),
     FEE_FIRST_BOUNDARY_TIME(30),
     FEE_FIRST_BOUNDARY_FEE(1000),
-    FEE_PER_TEN_MINUTES(400);
+    FEE_PER_TEN_MINUTES(500);
 
     public int constants;
 
